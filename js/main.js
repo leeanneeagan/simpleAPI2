43131e75d619bf51d45667c87cfa1cf35d3ef2f9
leeanneeagan <leeanne.eagan@gmail.com>
2025-10-05 20:03:22 -0400

    finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+// api TV show https://openwhyd.org/username?format=json.
+//
+///
+//
+//
+//call a search by show for info on show
 
+document.querySelector('button').onclick = searchSHOW
+
+
+function searchSHOW() {
+  const input = document.querySelector('input').value;
+  const url = `https://api.tvmaze.com/search/shows?q="${input}`;
+
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data => {
+      console.log(data);
+      document.querySelector('h2').innerText = data[0].show.name;
+    document.querySelector('h3').innerText = data[0].show.genres[0];
+    document.querySelector('h3').innerText = data[0].show.genres[1];
+    document.querySelector('img').src = data[0].show.image.medium;
+
+    }))
+    .catch((err) => console.error(err));
+
+
+}
